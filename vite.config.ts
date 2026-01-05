682fb4fef716545e4be31f7e03f43a394fa50e6c
frank.bassard <frank.bassard@gameloft.com>
2026-01-05 13:12:13 -0500

    Change base

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // TODO: Set this to your repository name for GitHub Pages
-  // base: '/your-repo-name/',
+  base: '/phoenix-form/',
 })
